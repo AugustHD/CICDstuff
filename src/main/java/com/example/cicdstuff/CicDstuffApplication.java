c65f6ea9c23ae8adf514a40346e486d6b4f1bd6d
augu0577 <august.hawk@gmail.com>
2023-01-04 23:22:27 +0100

    Initial commit

---
+++ src/main/java/com/example/cicdstuff/CicDstuffApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdstuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicDstuffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicDstuffApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdstuff/CicDstuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdstuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicDstuffApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
